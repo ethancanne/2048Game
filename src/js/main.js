86e3005204c6c1da0ea503a78c77968639cd97b2
Ethan Cannelongo <ethancannelongo@Ethans-MacBook-Pro.local>
2022-01-14 08:53:47 -0500

    Added arrow listeners

--- src/js/main.js
+++ src/js/main.js
@@ -1,23 +1,49 @@
 const data = [
-  [2, 2, "b", "b"],
+  [4, "b", "b", "b"],
   [2, 16, "b", "b"],
   [2, 4, "b", "b"],
   [2, 4, "b", "b"],
 ];
+
 const board = document.getElementsByClassName("board")[0];
 
-data.forEach(row => {
-  const rowElement = document.createElement("div");
-  rowElement.className = "row";
+//rerender the board from the data array
+const rerenderBoard = () => {
+  board.innerHTML = "";
+  data.forEach(row => {
+    const rowElement = document.createElement("div");
+    rowElement.className = "row";
 
-  row.forEach(item => {
-    const tile = document.createElement("div");
-    if (item == "b") tile.textContent = "";
-    else tile.textContent = item;
-    tile.className = "tile";
-    rowElement.appendChild(tile);
+    row.forEach(item => {
+      const tile = document.createElement("div");
+      if (item == "b") tile.textContent = "";
+      else tile.textContent = item;
+      tile.className = "tile";
+      rowElement.appendChild(tile);
+    });
+    board.appendChild(rowElement);
   });
-  board.appendChild(rowElement);
-});
+};
+rerenderBoard();
 
-console.log("hello");
+const checkKey = e => {
+  e = e || window.event;
+
+  if (e.keyCode == "38") {
+    console.log("Up");
+  } else if (e.keyCode == "40") {
+    console.log("Down");
+
+    // down arrow
+  } else if (e.keyCode == "37") {
+    console.log("Left");
+
+    // left arrow
+  } else if (e.keyCode == "39") {
+    console.log("Right");
+
+    // right arrow
+  }
+};
+
+document.onkeydown = checkKey;
