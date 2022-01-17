034d839829067e9e580145bc2975eaf133b72590
ZeNo20059 <95366018+ZeNo20059@users.noreply.github.com>
2022-01-17 10:15:13 -0500

    Added Left functionality / Misc
    Added: move.js now controls the left arrow keycode and shifts, matches, and shifts the values to the left.
    Trivial: Modified variable names and looping method.

---
+++ js/move.js
@@ -0,0 +1,139 @@
+export const up = board => {
+  console.log("up");
+  var match = false;
+  return match;
+};
+export const down = board => {
+  console.log("down");
+  var match = false;
+  return match;
+};
+
+/***********************
+ * Shift values to left
+ * Match values
+ * Shift values to left
+ **********************/
+
+export const left = board => {
+  // Declare variables
+  var shift = false;
+  var match = false;
+  var repeated = false;
+
+  // Use do...while() for a breakable "if" statement
+  do {
+    // Run through each available row
+    for (var row = 0; row < board.length; row++) {
+      // Shift the available values to the left
+      shift = true;
+      while (shift) {
+        // Iterate through each column checking the value next to it
+        shift = false;
+        for (var col = 1; col < board[row].length; col++) {
+          // Check if the column to the left is empty and the right is not-empty
+          if ((board[row][col - 1] === 0) && (board[row][col] !== 0)) {
+            board[row][col - 1] = board[row][col];
+            board[row][col] = 0;
+            shift = true;
+          }
+        }
+      }
+    }
+
+    // Break from do...while() when loop has repeated
+    if (repeated) {
+      repeated = false;
+      break;
+    }
+
+    // Match the values
+    for (var row = 0; row < board.length; row++) {
+      // Match each column by the value to the left
+      for (var col = 0; col < board.length - 1; col++) {
+        // Validate the next column exists
+        if (col + 1 === 4) {
+          break;
+        }
+
+        // Do comparisons using [col + 1] for previous element
+        if (board[row][col + 1] === board[row][col]) {
+          board[row][col] = board[row][col] * 2;
+          board[row][col + 1] = 0;
+          match = true;
+        }
+      }
+    }
+
+    // Repeat run when fully executed
+    repeated = true;
+  } while (repeated); // End while() when broken prematurely
+
+  // Return whether or not a successful match has been made
+  return match;
+};
+
+/***********************
+ * Shift values to right
+ * Match values
+ * Shift values to right
+ **********************/
+
+export const right = board => {
+  // Declare variables
+  var shift = false;
+  var match = false;
+  var repeated = false;
+
+  // Use do...while() for a breakable "if" statement
+  do {
+    // Run through each available row
+    for (var row = 0; row < board.length; row++) {
+      // Shift the available values to the right
+      shift = true;
+      while (shift) {
+        // Iterate through each column checking the value next to it
+        shift = false;
+        for (var col = board[row].length - 2; col > -1; col--) {
+          // Check if the column to the right is empty and the left is not-empty
+          if ((board[row][col + 1] === 0) && (board[row][col] !== 0)) {
+            board[row][col + 1] = board[row][col];
+            board[row][col] = 0;
+            shift = true;
+          }
+        }
+      }
+    }
+
+    // Break from do...while() when loop has repeated
+    if (repeated) {
+      repeated = false;
+      break;
+    }
+
+    // Match the values
+    for (var row = 0; row < board.length; row++) {
+      // Match each column by the value to the left
+      for (var col = board.length - 1; col > -1; col--) {
+        // Validate the previous column exists
+        if (col - 1 === -1) {
+          break;
+        }
+
+        // Do comparisons using [col - 1] for previous element
+        if (board[row][col - 1] === board[row][col]) {
+          board[row][col] = board[row][col] * 2;
+          board[row][col - 1] = 0;
+          match = true;
+        }
+      }
+    }
+
+    // Repeat run when fully executed
+    repeated = true;
+  } while (repeated); // End while() when broken prematurely
+
+  // Return whether or not a successful match has been made
+  return match;
+};
+export const generateTile = board => {};
